4a0a236575a0a19bb0b059412c875ad02234a5d0
Geoffroy Mounier <geoffroy.mounier@monkeymoney.fr>
2019-07-24 10:15:58 +0200

    ...

--- src/server/ecosystem.config.js
+++ src/server/ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps : [
       {
         name: "Vino",
-        script: "./app.js",
+        script: "./index.js",
         watch: true,
         env: {
             "PORT": 8080,//you can choose
