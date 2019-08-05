fdc13e38fc8c73713eadc152bbfe55f480c20320
Geoffroy Mounier <geoffroy.mounier@monkeymoney.fr>
2019-08-05 17:55:14 +0200

    ...

--- src/server/routes/mailMethods.js
+++ src/server/routes/mailMethods.js
@@ -20,7 +20,7 @@ module.exports = {
       let address = process.env.mode=="test" ? "https://"+hostname : "http://localhost:3000";
       let link = address+"/confirmMail?userId="+userId+"&token="+token;
       let options = {
-        from : "Vinologie <noreply@vinologie.fr>",
+        // from : "Vinologie <noreply@vinologie.fr>",
         to: email,//email
         subject: "Confirmer votre email pour [Vinologie]",
         html: "<div><p><strong>Veuillez cliquer sur ce lien pour valider que vous êtes le propriétaire de ce compte </strong></p><br/><p>Veuillez cliquer sur ce lien </p><br/>"
@@ -46,7 +46,7 @@ module.exports = {
       let link = address+"/resetPass?userId="+userId+"&token="+token;
 
       let options = {
-        from : "Vinologie <noreply@vinologie.fr>",
+        // from : "Vinologie <noreply@vinologie.fr>",
         to: email,//email
         subject: "Lien pour nouveau mot de passe [Vinologie]",
         html: "<div><p><strong>Changement de mot de passe pour Vinologie</strong></p><br/><p>Veuillez cliquer sur ce lien pour acc&eacute;der au formulaire de changement de mot de passe :</p><br/>"
