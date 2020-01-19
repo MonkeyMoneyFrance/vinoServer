ab8963d7104886278b34032d4353cb0845d9e13c
Geoffroy Mounier <geoffroymounier@gmail.com>
2020-01-19 20:08:11 +0000

    no message

--- src/server/core/auth.js
+++ src/server/core/auth.js
@@ -2,6 +2,9 @@ const Auth = require('../models/auth.js')
 const passport = require('passport')
 const FacebookStrategy =require('passport-facebook').Strategy;
 const {signRequestToken} = require('../routes/middlewares')
+const {createResetPasswordToken} = require('../routes/redismethods.js')
+const {sendConfirmMail} = require('../routes/mailMethods.js');
+
 module.exports = {
   findAuth : (_id) => {
     console.log('TRU TO FIND')
@@ -122,98 +125,99 @@ module.exports = {
 
     },
 
-  googleAuth : (accessToken, refreshToken, profile, done) => {
-      Auth.findOne({
-            'email': profile.emails[0].value
-      }, function(err, auth) {
-        if (err) done(err)
-        // no user was found, lets create a new one
-        if (!auth) {
-              Auth.create({
-                    email: profile.emails[0].value,
-                    name : profile.displayName,
-                    googleProvider: {
-                          id: profile.id,
-                          token: accessToken
-                    }
-              },{new:true,upsert:true}).then((createdAuth)=>{
-                // const token = signRequestToken({userId:createdAuth._id.toString()})
-                done(null,createdAuth._id.toString())
-                // resolve({...profile,token,userId:createdAuth._id.toString(),accessToken})
-              }).catch((error)=>{
-                done(error)
-              })
-        } else if (!auth.googleProvider) {
-
-            Auth.updateOne({email:profile.emails[0].value},{
-              googleProvider: {
-                    id: profile.id,
-                    token: accessToken
-              }
-            }).then(()=>{
-              // const token = signRequestToken({userId:auth._id.toString()})
-              done(null,auth._id.toString())
-              // resolve({...profile,token,userId:auth._id.toString(),accessToken})
-            }).catch((error)=>{
-              done(error)
-            })
-
-        } else {
-          // const token = signRequestToken({userId:auth._id.toString()})
-          done(null,auth._id.toString())
-          // resolve({...profile,token,userId:auth._id.toString(),accessToken})
-        }
-      });
-
-  },
-  facebookAuth : (accessToken, refreshToken, profile, done) => {
-          Auth.findOne({
-                'email': profile.emails[0].value
-          }, function(err, auth) {
-            if (err) done(err)
-            // no user was found, lets create a new one
-            if (!auth) {
-                  Auth.create({
+    googleAuth : (accessToken, refreshToken, profile, done) => {
+      console.log(accessToken, refreshToken, profile)
+        Auth.findOne({
+              'email': profile.emails[0].value
+        }, function(err, auth) {
+          if (err) done(err)
+          // no user was found, lets create a new one
+          if (!auth) {
+                Auth.create({
                       email: profile.emails[0].value,
                       name : profile.displayName,
-                      facebookProvider: {
+                      googleProvider: {
                             id: profile.id,
                             token: accessToken
                       }
-                  },{new:true,upsert:true}).then((createdAuth)=>{
-                    // const token = signRequestToken({userId:createdAuth._id.toString()})
-                    done(null,createdAuth._id.toString())
-                  }).catch((error)=>{
-                    console.log(error)
-                    done(error)
-                  })
-            } else if (!auth.facebookProvider) {
-
-                Auth.updateOne({email:profile.emails[0].value},{
-                  facebookProvider: {
-                        id: profile.id,
-                        token: accessToken
-                  }
-                }).then(()=>{
-                  // const token = signRequestToken({userId:auth._id.toString()})
-                  done(null,auth._id.toString())
-                  // resolve({...profile,token,userId:auth._id.toString(),accessToken})
+                }).then((createdAuth)=>{
+                  // const token = signRequestToken({userId:createdAuth._id.toString()})
+                  done(null,createdAuth._id.toString())
+                  // resolve({...profile,token,userId:createdAuth._id.toString(),accessToken})
                 }).catch((error)=>{
                   done(error)
                 })
+          } else if (!auth.googleProvider) {
 
-            } else {
-              done(null,auth._id.toString())
-            }
-          });
+              Auth.updateOne({email:profile.emails[0].value},{
+                googleProvider: {
+                      id: profile.id,
+                      token: accessToken
+                }
+              }).then(()=>{
+                // const token = signRequestToken({userId:auth._id.toString()})
+                done(null,auth._id.toString())
+                // resolve({...profile,token,userId:auth._id.toString(),accessToken})
+              }).catch((error)=>{
+                done(error)
+              })
 
-  },
-  delete : (email) => {
-    return new Promise(function(resolve,reject){
-      Auth.findOneAndRemove({email}).then((response)=>resolve(response))
-      .catch((error)=>reject(error))
-    })
-  }
+          } else {
+            // const token = signRequestToken({userId:auth._id.toString()})
+            done(null,auth._id.toString())
+            // resolve({...profile,token,userId:auth._id.toString(),accessToken})
+          }
+        });
+
+    },
+    facebookAuth : (accessToken, refreshToken, profile, done) => {
+            Auth.findOne({
+                  'email': profile.emails[0].value
+            }, function(err, auth) {
+              if (err) done(err)
+              // no user was found, lets create a new one
+              if (!auth) {
+                    Auth.create({
+                        email: profile.emails[0].value,
+                        name : profile.displayName,
+                        facebookProvider: {
+                              id: profile.id,
+                              token: accessToken
+                        }
+                    }).then((createdAuth)=>{
+                      // const token = signRequestToken({userId:createdAuth._id.toString()})
+                      done(null,createdAuth._id.toString())
+                    }).catch((error)=>{
+                      console.log(error)
+                      done(error)
+                    })
+              } else if (!auth.facebookProvider) {
+
+                  Auth.updateOne({email:profile.emails[0].value},{
+                    facebookProvider: {
+                          id: profile.id,
+                          token: accessToken
+                    }
+                  }).then(()=>{
+                    // const token = signRequestToken({userId:auth._id.toString()})
+                    done(null,auth._id.toString())
+                    // resolve({...profile,token,userId:auth._id.toString(),accessToken})
+                  }).catch((error)=>{
+                    done(error)
+                  })
+
+              } else {
+                done(null,auth._id.toString())
+              }
+            });
+
+    },
+    delete : (email) => {
+      return new Promise(function(resolve,reject){
+        Auth.findOneAndRemove({email}).then((response)=>resolve(response))
+        .catch((error)=>reject(error))
+      })
+    }
 
 
 
