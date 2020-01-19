abcb4f4021f3146b1e8a4fe39e4c0b3ac3a0f1a3
Geoffroy Mounier <geoffroymounier@gmail.com>
2020-01-19 20:06:00 +0000

    no message

--- src/server/core/auth.js
+++ src/server/core/auth.js
@@ -16,6 +16,112 @@ module.exports = {
       })
     })
   },
+  askForConfirmation : (req,id,email) => {
+    return new Promise(async (resolve,reject) => {
+      console.log(req.hostname,email,id)
+      try {
+        let passwordToken = await createResetPasswordToken(id)
+        await sendConfirmMail(req.hostname,email,id,passwordToken)
+        console.log('DID SEND MSG')
+        resolve()
+      } catch (err) {
+        console.log(err)
+        reject(err)
+      }
+    })
+  },
+  confirmMail : (_id) => {
+    return new Promise(function(resolve,reject){
+
+      Auth.updateOne({_id : new  ObjectId(_id)},{
+         '$set': {"emailProvider.verify" : true}
+      }).then(update => {
+        console.log(update)
+        resolve()
+      }).catch((err)=>{
+        console.log("User not found, error: "+err)
+        reject("User not found, error: "+err)
+        return;
+      })
+    })
+  },
+  resetPass : (_id,password) => {
+    return new Promise(function(resolve,reject){
+      Auth.findOne({_id:new  ObjectId(_id)}).then(auth => {
+        auth.emailProvider =  {...auth.emailProvider,password}
+        auth.save(function(err){
+          if(err) return reject(err)
+          resolve();
+        })
+      }).catch((err)=>{
+        console.log("User not found, error: "+err)
+        reject("User not found, error: "+err)
+        return;
+      })
+    })
+  },
+  findEmail : (email) => {
+    return new Promise((resolve,reject) => {
+      Auth.findOne({'$and' : [
+        {email},
+        {emailProvider : {$exists:true}}
+      ] }).then((user)=>{
+        if (user == null){
+          resolve() //no user with this email
+        }
+        else{
+          resolve(user._id)
+        }
+      }).catch(err => {
+        reject(err)
+      })
+    })
+  },
+  localAuth : (req, email, password, done) => {
+      Auth.findOne({'$and' : [
+          {email: email},
+      ]}, {_id:1,emailProvider:1} , (err, auth) => {
+        if (err) done(err)
+        if (!auth && req.body && req.body.name) {
+              Auth.create({
+                  name : req.body.name,
+                  email: email,
+                  emailProvider : {password,verify:false}
+              }).then(createdAuth=>{
+                  self.askForConfirmation(req,createdAuth._id,email)
+                  .then(()=>done("please verify",null,"please verify"))
+                  .catch((err) => done(err))
+
+              }).catch((error)=>{
+                done(error)
+              })
+        } else if (auth && !auth.emailProvider && req.body && req.body.name) {
+            auth.emailProvider =  {verify:false,password}
+            auth.save(async (err) => {
+              if(err) done(error)
+              self.askForConfirmation(req,auth._id,email)
+              .then(()=>done("please verify",null,"please verify"))
+              .catch((err) => done(err))
+            })
+        } else if (auth && !auth.emailProvider) {
+          return done("no linked account",null)
+        } else if (auth && !auth.emailProvider.verify == true) {
+          return !req.body || !req.body.name ? done("not verified") : done('unauthorized')
+        } else if (auth && !req.body.name){
+          auth.comparePassword(password,(err,isMatch)=>{
+            if(isMatch){
+              done(null,auth._id.toString())
+            } else {
+              done('wrong password')
+            }
+          })
+        } else {
+           done('unauthorized')
+        }
+      })
+
+    },
+
   googleAuth : (accessToken, refreshToken, profile, done) => {
       Auth.findOne({
             'email': profile.emails[0].value
